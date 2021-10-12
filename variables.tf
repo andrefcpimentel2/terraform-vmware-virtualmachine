46906f49bdac0c543542da25ffed3694f737e2d5
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2021-10-12 11:11:19 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "vsphere_user" {
+description = "Vsphere user"
+}
+ 
+variable "vsphere_password" {
+description = "Vsphere password"
+}
+ 
+variable "vsphere_server" {
+description = "Vsphere Server"
+}
